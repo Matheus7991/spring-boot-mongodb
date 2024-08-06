88d02b56135b11f29e5e15df4a7382566666ca4b
Matheus Freitas <bandeira.freitas2013@gmail.com>
2024-08-06 16:31:58 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/matheusfreitas/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.matheusfreitas.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.matheusfreitas.workshopmongo.domain.User;
+import com.matheusfreitas.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+}
