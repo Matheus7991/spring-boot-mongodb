d523c6aed4213c4d04de745951cda17b05140608
unknown <bandeira.freitas2013@gmail.com>
2024-08-11 11:52:28 -0300

    Insercao de usuario com POST

--- src/main/java/com/matheusfreitas/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusfreitas/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusfreitas.workshopmongo.domain.User;
+import com.matheusfreitas.workshopmongo.dto.UserDTO;
 import com.matheusfreitas.workshopmongo.repository.UserRepository;
 import com.matheusfreitas.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -26,5 +27,13 @@ public class UserService {
 	    	throw new ObjectNotFoundException("Objeto não encontrado");
 	    }
 	    return optUser.get();
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
