dcf4d8e37d5d90c5786f21021b49df7630db0ca6
unknown <bandeira.freitas2013@gmail.com>
2024-08-11 11:38:01 -0300

    Obtendo usuário por id

--- src/main/java/com/matheusfreitas/workshopmongo/services/UserService.java
+++ src/main/java/com/matheusfreitas/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.matheusfreitas.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.matheusfreitas.workshopmongo.domain.User;
 import com.matheusfreitas.workshopmongo.repository.UserRepository;
+import com.matheusfreitas.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+	    Optional<User> optUser = repo.findById(id); // returns java8 optional
+	    if (!optUser.isPresent()) {
+	    	throw new ObjectNotFoundException("Objeto não encontrado");
+	    }
+	    return optUser.get();
 	}
 }
